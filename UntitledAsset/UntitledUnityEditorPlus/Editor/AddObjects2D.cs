5b808bfff2db4208809cf4525d9914ac21bbea63
toryGO <157636498+toryGO@users.noreply.github.com>
2024-01-29 17:08:33 +0100

    Update AddObjects2D.cs

--- UntitledAsset/UntitledUnityEditorPlus/Editor/AddObjects2D.cs
+++ UntitledAsset/UntitledUnityEditorPlus/Editor/AddObjects2D.cs
@@ -57,58 +57,5 @@ namespace UntitledAsset.UntitledEditor
 				}
 			}
 		}
-
-
-		private static GameObject enemyAttackZoneTriggerPrefab;
-
-		[MenuItem("GameObject/Untitled Editor/Add Enemy Attack Zone Trigger 2D", false, 0)]
-		public static void EnemyAttackZoneTrigger(MenuCommand menuCommand)
-		{
-			if (enemyAttackZoneTriggerPrefab == null)
-			{
-				enemyAttackZoneTriggerPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/UntitledAsset/UntitledObjects/Attack Zone Trigger.prefab");
-			}
-			
-			if (enemyAttackZoneTriggerPrefab != null)
-			{
-				GameObject selectedObject = menuCommand.context as GameObject;
-				if (selectedObject != null)
-				{
-					GameObject enemyAttackZoneTriggerInstance = PrefabUtility.InstantiatePrefab(enemyAttackZoneTriggerPrefab) as GameObject;
-					if (enemyAttackZoneTriggerInstance != null)
-					{
-						Undo.RegisterCreatedObjectUndo(enemyAttackZoneTriggerInstance, "Undo Add Enemy Attack Zone Trigger 2D");
-						enemyAttackZoneTriggerInstance.transform.parent = selectedObject.transform;
-						enemyAttackZoneTriggerInstance.transform.localPosition = Vector3.zero;
-					}
-				}
-			}
-		}
-
-		private static GameObject enemyEnterTriggerPrefab;
-
-		[MenuItem("GameObject/Untitled Editor/Add Enemy Enter Trigger 2D", false, 0)]
-		public static void EnemyEnterTrigger(MenuCommand menuCommand)
-		{
-			if (enemyEnterTriggerPrefab == null)
-			{
-				enemyEnterTriggerPrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/UntitledAsset/UntitledObjects/Enter Trigger.prefab");
-			}
-			
-			if (enemyEnterTriggerPrefab != null)
-			{
-				GameObject selectedObject = menuCommand.context as GameObject;
-				if (selectedObject != null)
-				{
-					GameObject enemyEnterTriggerInstance = PrefabUtility.InstantiatePrefab(enemyEnterTriggerPrefab) as GameObject;
-					if (enemyEnterTriggerInstance != null)
-					{
-						Undo.RegisterCreatedObjectUndo(enemyEnterTriggerInstance, "Undo Add Enemy Enter Trigger 2D");
-						enemyEnterTriggerInstance.transform.parent = selectedObject.transform;
-						enemyEnterTriggerInstance.transform.localPosition = Vector3.zero;
-					}
-				}
-			}
-		}
 	}
 }
